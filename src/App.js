4bc1832bcc90817df8b0455422ff6d537852b5be
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-29 11:12:39 +0400

    basic styling and adding todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [toDos, settoDos] = useState([]); //array to store the state of list of items
+  const [toDo, settoDo] = useState(""); //to store the state each individual tast which is stored as string
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
     </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input type="text" onChange={(e) => settoDo(e.target.value)} placeholder="🖊️ Add item..." /> {/* taking each list item from the input and storing in setodo */}
+      <i onClick={() => settoDos([...toDos,toDo])} className="fas fa-plus"></i> {/* on clicking the add button, use the spread operator to list the todos one by one and adding to the list */}
+    </div>
+    <div className="todos">
+
+
+    {
+      toDos.map((value)=> { 
+        return(
+      <div className="todo">
+      <div className="left">
+        <input type="checkbox" name="" id="" />
+        <p>{value}</p>
+      </div>
+      <div className="right">
+        <i className="fas fa-times"></i>
+      </div>
+    </div>
+        );
+    })
+    }
+    </div>
+  </div>
   );
 }
 
